341c5cab9fe1f424d2fbf020b0f61063b0e71d6e
anirudhk06 <anirudhani300@gmail.com>
2025-06-15 16:35:50 +0530

    optimized way

--- creational-design/singleton/singleton.py
+++ creational-design/singleton/singleton.py
@@ -6,10 +6,11 @@ class SingletonMeta(type):
     _lock = threading.Lock()
 
     def __call__(cls, *args, **kwargs):
-        with cls._lock:
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
+        if cls not in cls._instances:
+            with cls._lock:
+                if cls not in cls._instances:
+                    instance = super().__call__(*args, **kwargs)
+                    cls._instances[cls] = instance
         return cls._instances[cls]
 
 
